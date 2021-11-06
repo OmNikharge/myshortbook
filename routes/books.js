89404c649a77670d492a55410947f855060419f1
Om Nikharge <omnikharge@gmail.com>
2021-11-06 17:59:16 +0530

    Backend Finished

--- routes/books.js
+++ routes/books.js
@@ -37,40 +37,101 @@ const { query } = require('express')
 
 // All Books Routes
 router.get('/', async (req, res) => {
-    let query = Book.find() 
-    if(req.query.title != null && req.query.title != ''){     
+    let query = Book.find()
+    if (req.query.title != null && req.query.title != '') {
         // regex() Selects documents where values match a specified regular expression.
-         query= query.regex('title', new RegExp(req.query.title,'i'))
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
     }
-    if(req.query.readBefore != null && req.query.readBefore != ''){
+    if (req.query.readBefore != null && req.query.readBefore != '') {
         //Matches values that are less than or equal to a specified value.
-         query= query.lte('read_date', new RegExp(req.query.readBefore))
+        query = query.lte('read_date', new RegExp(req.query.readBefore))
     }
-    if(req.query.readAfter != null && req.query.readAfter != ''){
+    if (req.query.readAfter != null && req.query.readAfter != '') {
         // Matches values that are greater than or equal to a specified value.
-         query= query.gte('read_date', new RegExp(req.query.readAfter))
+        query = query.gte('read_date', new RegExp(req.query.readAfter))
     }
-    try{
+    try {
         const books = await query.exec()
- res.render('books/index',{
-        books:books,
-        typedText: req.query
-    })
-    }catch{
+        res.render('books/index', {
+            books: books,
+            typedText: req.query
+        })
+    } catch {
         res.redirect('/')
     }
-   
+
 
 })
 
 // New Book Route
 router.get('/new', async (req, res) => {
-    renderNewPage(res, new Book())
+
+    renderNewFormPage(res, new Book(), 'new')
 })
 
-// Create Book Route
-router.post('/',  async (req, res) => {
+//Show Books Route
+router.get('/:id', async (req, res) => {
+    let book
+    try {
+        book = await Book.findById(req.params.id).populate('author').exec()
+        res.render('books/show', { book: book })
 
+    } catch {
+        res.redirect('/')
+    }
+})
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id)
+        renderNewFormPage(res, book, 'edit')
+    } catch {
+        res.redirect('/books')
+    }
+})
+//Update Book Route
+router.put('/:id', async (req, res) => {
+    let book
+    try {
+        book = await Book.findById(req.params.id)
+        book.title = req.body.title
+        book.author = req.body.author
+        book.read_date = req.body.read_date
+        book.pageCount = req.body.pageCount
+        book.mybooknotes = req.body.mybooknotes
+        if (req.body.cover != null && req.body.cover !== '') {
+            saveCover(book, req.body.cover)
+            await book.save()
+            res.redirect(`/books/${book.id}`)
+        } else {
+            renderNewFormPage(res, book, 'edit', true)
+        }
+    } catch {
+        res.redirect('/')
+    }
+})
+
+//Delete book Route
+router.delete('/:id', async (req,res)=>{
+   let book
+   try {
+       book= await Book.findById(req.params.id)
+       book.remove()
+       res.redirect('/books')
+   } catch{
+       if(book != null){
+           res.render('books/show',{
+               book:book,
+               errorMessage: 'Could not remove Book'
+           })
+       } else{
+           res.redirect('/')
+       }
+   }
+
+})
+// Create Book Route
+router.post('/', async (req, res) => {
     const book = new Book({
         title: req.body.title,
         author: req.body.author,
@@ -79,39 +140,51 @@ router.post('/',  async (req, res) => {
         pageCount: req.body.pageCount,
         mybooknotes: req.body.mybooknotes
     })
-      saveCover(book, req.body.cover);
+    saveCover(book, req.body.cover);
     try {
-        const newBook = await book.save()                                                  
-        res.redirect(`books`)
+
+        const newBook = await book.save()
+        res.redirect('/books')
     } catch {
-        renderNewPage(res, new Book(), true)
+        renderNewFormPage(res, new Book(), 'new', true)
     }
 })
 
- 
-
-async function renderNewPage(res, book, hasError = false) {
-    try { 
+//This Function is for rendering new pBook page and Edit Book Page respectively.
+async function renderNewFormPage(res, book, form, hasError = false) {
+    try {
         const authors = await Author.find({})
         const params = {
             authors: authors,
             book: book
         }
-        if (hasError) { params.errorMessage = 'Error Creating Book' }
-        res.render('books/new', params)
+
+        if (hasError) {
+            if (form === 'edit') {
+            params.errorMessage = 'Error Updating Book'
+            } else{ params.errorMessage = 'Error Creating Book'}
+        }
+        res.render(`books/${form}`, params)
     }
     catch {
         res.redirect('/books')
     }
 }
+
+
 const imageMimeTypes = ['image/jpeg', 'image/png']
-function saveCover(book, coverEncoded){
-    if(coverEncoded == null) return
-    const cover = JSON.parse(coverEncoded) //Here we are converting the string which is internally a json into a json object
-    if(cover != null && imageMimeTypes.includes(cover.type)){
+
+function saveCover(book, coverEncoded) {
+    if (coverEncoded == null) return
+
+    const cover = JSON.parse(coverEncoded)
+    //Here we are converting the string which is internally a json into a json object
+    if (cover != null && imageMimeTypes.includes(cover.type)) {
+
         book.coverImage = new Buffer.from(cover.data, 'base64')
         book.coverImageType = cover.type
     }
 }
+
 module.exports = router;
 
